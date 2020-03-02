0bbcdcc0f656011512f22c425a819fc3d644adb0
Silvan Paul <silvan.paul123@gmail.com>
2020-03-02 11:17:14 +0100

    added dot env support for the config files

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// load enviorment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// set enviorment variables
+	version := os.Getenv("version")
+	botID := os.Getenv("client_id")
+	// log a fancy ascii art with botId and Version number
+	logoArt := fmt.Sprintf(`
+    =============================================================================
+        _    _ _ _          _ ____        _   
+       / \  | | (_) ___  __| | __ )  ___ | |_ 
+      / _ \ | | | |/ _ \/ _  |  _ \ / _ \| __|
+     / ___ \| | | |  __/ (_| | |_) | (_) | |_ 
+    /_/   \_\_|_|_|\___|\__,_|____/ \___/ \__|
+	
+    Version: %s
+    BotId: %s
+    =============================================================================
+   `, version, botID)
+	print(logoArt)
 }
